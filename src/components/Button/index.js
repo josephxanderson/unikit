919f1fd75214cdddef3d81525d6624cdaf5ae3a0
Joseph Anderson <josephxanderson@icloud.com>
2021-05-03 11:21:02 -0400

    Main button functionality implemented.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,17 +1,12 @@
 // Framework imports
-import React, { useContext } from 'react';
+import React from 'react';
 
 // Application imports
 import './styles.scss';
-import ConfigurationContext from '../../configuration-context.js';
+import Icon from '../Icon';
 
-const getElementProperties = (backgroundColor, color, style) => {
+const getElementProperties = (color, style) => {
 	let className = "button";
-
-	if (backgroundColor) {
-		// Add the background color classname.
-		className = className.concat(` backgroundColor-${backgroundColor}`);
-	}
 
 	if (color) {
 		// Add the color classname.
@@ -30,18 +25,39 @@ const getElementProperties = (backgroundColor, color, style) => {
 	];
 }
 
+const getIcon = (icon) => {
+	// Determine if the image is an application provided icon.
+	// To do so, we check if the icon name provided begins with a '/'.
+	// If so, that means it's not an application provided icon,
+	// and we should use the Icon imagePath property.
+	if (
+		icon.startsWith('../') ||
+		icon.startsWith('./') ||
+		icon.startsWith('/')
+	) {
+		return (<Icon imagePath={icon}/>);
+	} else {
+		return (<Icon name={icon}/>);
+	}
+}
+
 const Button = ({
 	accessibilityLabel,
-	backgroundColor,
 	color,
-	image,
+	icon,
 	title,
 	style,
 }) => {
-	const [className] = getElementProperties(backgroundColor, color, style);
+	const [className] = getElementProperties(color, style);
 
 	const element = (
 		<button className={className} role="button" aria-label={accessibilityLabel}>
+			{
+				icon
+				? getIcon(icon)
+				: null
+			}
+
 			<span className="button-title">{title}</span>
 		</button>
 	);
---
+++ src/components/Icon/index.js
@@ -0,0 +1,59 @@
+// Framework imports
+import React from 'react';
+
+// Application imports
+import './styles.scss';
+import icons from './icons';
+
+const getElementProperties = (color, style) => {
+	let className = "icon";
+
+	if (color) {
+		// Add the color classname.
+		className = className.concat(` color-${color}`);
+	}
+
+	return [
+		className,
+	];
+}
+
+const Icon = ({
+	accessibilityLabel,
+	color,
+	name,
+	imagePath,
+}) => {
+	const [className] = getElementProperties(color, imagePath);
+
+	let element = null;
+
+	// If we have an imagePath, then this is a custom icon and we should get the
+	if (imagePath) {
+		element = (
+			<figure
+			className={className}
+			role="icon"
+			{...(accessibilityLabel ? {"aria-label": accessibilityLabel} : {})} // If accessibilityLabel then use aria-label.
+			{...(accessibilityLabel ? {} : {"aria-hidden": true})} // If no accessibility label then set aria-hidden="true".
+			style={{backgroundImage: `url(${imagePath})`}} />
+		);
+	} else {
+		element = (
+			<svg
+				className={className}
+				role="icon"
+				{...(accessibilityLabel ? {"aria-label": accessibilityLabel} : {})} // If accessibilityLabel then use aria-label.
+				{...(accessibilityLabel ? {} : {"aria-hidden": true})} // If no accessibility label then set aria-hidden="true".
+				width="32"
+				height="32"
+				viewBox="0 0 32 32">
+				<path d={icons[name]}></path>
+			</svg>
+		);
+	}
+
+	return element;
+}
+
+export default Icon;
