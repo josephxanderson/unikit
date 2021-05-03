d8562b29d9697bf82fa008d2edd66e1f94886e4e
Joseph Anderson <josephxanderson@icloud.com>
2021-05-02 23:48:06 -0400

    Continuing component development.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,52 @@
+// Framework imports
 import React, { useContext } from 'react';
 
+// Application imports
+import './styles.scss';
 import ConfigurationContext from '../../configuration-context.js';
 
-const Button = (props) => {
-	return (
-		<ConfigurationContext.Consumer>
-			{value => (
-				<button>{value.accentColor}</button>
-			)}
-		</ConfigurationContext.Consumer>
+const getElementProperties = (backgroundColor, color, type) => {
+	let className = "button";
+
+	if (backgroundColor) {
+		// Add the background color classname.
+		className = className.concat(` backgroundColor-${backgroundColor}`);
+	}
+
+	if (color) {
+		// Add the color classname.
+		className = className.concat(` color-${color}`);
+	}
+
+	if (type) {
+		type.forEach((type) => {
+			// Add the type classname.
+			className = className.concat(` type-${type}`);
+		})
+	}
+
+	return [
+		className,
+	];
+}
+
+const Button = ({
+	accessibilityLabel,
+	backgroundColor,
+	color,
+	image,
+	title,
+	type,
+}) => {
+	const [className] = getElementProperties(backgroundColor, color, type);
+
+	const element = (
+		<button className={className} role="button" aria-label={accessibilityLabel}>
+			<span className="button-title">{title}</span>
+		</button>
 	);
+
+	return element;
 }
 
 export default Button;
--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -4,6 +4,95 @@ import PropTypes from "prop-types";
 
 import ConfigurationContext from "../../configuration-context.js";
 
+const getElementProperties = (alignment, color, preset, style) => {
+	let className = "text";
+	let role = null;
+	let Tag = null;
+
+	if (preset) {
+		className = className.concat(` style-${preset}`);
+
+		switch (preset) {
+			case "largeTitle":
+				role = "heading";
+				Tag = "h1";
+				break;
+
+			case "title1":
+				role = "heading";
+				Tag = "h2";
+				break;
+
+			case "title2":
+				role = "heading";
+				Tag = "h3";
+				break;
+
+			case "title3":
+				role = "heading";
+				Tag = "h4";
+				break;
+
+			case "headline":
+				role = "heading";
+				Tag = "p";
+				break;
+
+			case "body":
+				role = "unknown";
+				Tag = "p";
+				break;
+
+			case "callout":
+				role = "unknown";
+				Tag = "p";
+				break;
+
+			case "subhead":
+				role = "unknown";
+				Tag = "p";
+				break;
+
+			case "footnote":
+				role = "unknown";
+				Tag = "span";
+				break;
+
+			case "caption1":
+				role = "unknown";
+				Tag = "h5";
+				break;
+
+			case "caption2":
+				role = "unknown";
+				Tag = "h6";
+				break;
+
+			default:
+				role = "unknown";
+				Tag = "p";
+		}
+	} else {
+		className = className.concat(` style-body`);
+	}
+
+	if (color) {
+		// Add the color classname.
+		className = className.concat(` color-${color}`);
+	}
+
+	if (alignment) {
+		// Add the alignment classname.
+		className = className.concat(` align-${alignment}`);
+	}
+
+	return [
+		className,
+		role,
+		Tag
+	];
+}
+
 const Text = ({
 	alignment,
 	children,
@@ -11,29 +100,9 @@ const Text = ({
 	preset,
 	style,
 }) => {
-	let role = null;
-	let Tag = "span";
+	const [className, role, Tag] = getElementProperties(alignment, color, preset, style);
 
-	const determinePreset = () => {
-		if (preset == "largeTitle") {
-			role = "heading";
-			Tag = "h1";
-		}
-
-		if (preset == "title1") {
-			role = "heading";
-			Tag = "h2";
-		}
-	}
-
-	determinePreset();
-
-	const element = (<Tag className={`
-		text
-		style--${preset}
-		${color ? 'color--${color}' : null}
-		${alignment ? 'align--${alignment}' : null}
-	`} role={role}>{children}</Tag>);
+	const element = (<Tag className={className} role={role}>{children}</Tag>);
 
 	return element;
 }
